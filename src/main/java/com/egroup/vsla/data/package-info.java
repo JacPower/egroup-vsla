c02349d5918b585ed73c51823b5ec8899ddd3f9c
start.vaadin.com <no-reply@vaadin.com>
2024-03-24 20:09:12 +0000

    Generated project

---
+++ src/main/java/com/egroup/vsla/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.egroup.vsla.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/egroup/vsla/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.egroup.vsla.services;
+
+import org.springframework.lang.NonNullApi;
